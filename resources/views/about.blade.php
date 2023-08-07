bf305ecc7714b6636c9439a375b759eab5904b45
LexAeterna731 <Delirium731@yandex.ru>
2023-08-07 17:38:28 +0700

    add layout

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,2 +1,7 @@
-<h1>О блоге</h1>
-<p>Эксперименты с Laravel на Хекслете</p>
+@extends('layouts.app')
+
+@section('title', 'О блоге')
+@section('content')
+    <h1>О блоге</h1>
+    <p>Эксперименты с Laravel на Хекслете</p>
+@endsection
