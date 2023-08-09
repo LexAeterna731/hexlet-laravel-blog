e3949501d1d9aa3761eba681bed2b394061055bb
LexAeterna731 <Delirium731@yandex.ru>
2023-08-09 23:29:00 +0700

    add article creation

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -7,8 +7,36 @@ use App\Models\Article;
 
 class ArticleController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         $articles = Article::paginate();
         return view('article.index', compact('articles'));
+    }
+
+    public function create()
+    {
+        $article = new Article();
+        return view('article.create', compact('article'));
+    }
+
+    public function store(Request $request)
+    {
+        $data = $this->validate($request, [
+            'name' => 'required|unique:articles',
+            'body' => 'required|min:1000'
+        ]);
+
+        $article = new Article();
+        $article->fill($data);
+        $article->save();
+
+        return redirect()->route('articles.index')
+            ->with('success', 'Task was successful!');
+    }
+
+    public function show($id)
+    {
+        $article = Article::findOrFail($id);
+        return view('article.show', compact('article'));
     }
 }
--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -1,9 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
+    @if ($message = Session::get('success'))
+    <div class="alert alert-success alert-block">
+        <button type="button" class="close" data-dismiss="alert">×</button>	
+            <strong>{{ $message }}</strong>
+    </div>
+    @endif
     <h1>Список статей</h1>
     @foreach ($articles as $article)
-        <h2>{{$article->name}}</h2>
+        <h2>
+            <a href="{{ route('articles.show', ['id' => $article->id]) }}">{{$article->name}}</a>
+        </h2>
         <div>{{Str::limit($article->body, 200)}}</div>
     @endforeach
 @endsection
